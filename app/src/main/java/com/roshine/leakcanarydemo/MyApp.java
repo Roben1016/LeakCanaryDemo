264fd1ebf9adad8e81140fcf289050d474a5a482
Roshine <931304703@qq.com>
2018-03-20 20:39:41 +0800

    first commit！

---
+++ app/src/main/java/com/roshine/leakcanarydemo/MyApp.java
@@ -0,0 +1,37 @@
+package com.roshine.leakcanarydemo;
+
+import android.app.Application;
+import android.os.StrictMode;
+
+import com.squareup.leakcanary.LeakCanary;
+
+/**
+ * @author Roshine
+ * @date 2018/3/20 20:28
+ * @blog http://www.roshine.xyz
+ * @email roshines1016@gmail.com
+ * @github https://github.com/Roben1016
+ * @phone 136****1535
+ * @desc
+ */
+public class MyApp extends Application {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            // This process is dedicated to LeakCanary for heap analysis.
+            // You should not init your app in this process.
+            return;
+        }
+        enabledStrictMode();
+        LeakCanary.install(this);
+    }
+
+    private static void enabledStrictMode() {
+        StrictMode.setThreadPolicy(new StrictMode.ThreadPolicy.Builder() //
+                .detectAll() //
+                .penaltyLog() //
+                .penaltyDeath() //
+                .build());
+    }
+}
